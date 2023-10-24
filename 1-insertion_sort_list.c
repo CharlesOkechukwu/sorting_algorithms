87fb4dac26ea570974899f0b25faef76eb31d7be
CharlesOkechukwu <realcharlieok@gmail.com>
2023-10-24 15:13:00 -0700

    sort doubly linked list using insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -47,7 +47,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *first, *sec;
 
-	if (list == NULL || *list == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 	first = *list;
 	while (first != NULL)
