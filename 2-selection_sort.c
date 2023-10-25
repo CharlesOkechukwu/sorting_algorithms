991a8ff550835e585242cf03f0e58e308ba400e9
CharlesOkechukwu <realcharlieok@gmail.com>
2023-10-24 21:32:22 -0700

    selection sort algorthims

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,6 +13,8 @@ void selection_sort(int *array, size_t size)
 	int old, match;
 	size_t i, j;
 
+	if (size < 2 || !array)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = i + 1; j < size; j++)
