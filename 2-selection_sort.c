f61016217f82c9e2d3bfb2db695bcd9f1968517f
CharlesOkechukwu <realcharlieok@gmail.com>
2023-10-24 21:44:19 -0700

    selection sorting algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,22 +11,27 @@
 void selection_sort(int *array, size_t size)
 {
 	int old, match;
-	size_t i, j;
+	size_t i, j, min;
 
 	if (size < 2 || !array)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
+		min = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[i] > array[j])
+			if (array[min] > array[j])
 			{
-				old = array[i];
-				match = array[j];
-				array[i] = match;
-				array[j] = old;
+				min = j;
 			}
 		}
-		print_array(array, size);
+		if (min != i && array[min] < array[i])
+		{
+			old = array[i];
+			match = array[min];
+			array[i] = match;
+			array[min] = old;
+			print_array(array, size);
+		}
 	}
 }
